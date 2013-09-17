4f1d88c13846ccdb5715c57175226f229b697f02
Brad Aagaard <baagaard@usgs.gov>
2013-09-16 17:53:12 -0700

    Removed use of Python eggs.
    Use path to pyre module to create include path to mpi module rather than
    using eggs. This eliminates the dependence on merlin and archimedes.

--- setup.py
+++ setup.py
@@ -1,25 +1,17 @@
 
-from archimedes import use_merlin
-use_merlin()
-
-from merlin import setup, find_packages
+from distutils.core import setup
 
 setup(
     
     name = 'nemesis', 
-    version = '1.0',
+    version = '1.1',
 
     zip_safe = False,
-    packages = find_packages(),
     
-    install_requires = [
-    'pythia[mpi] >= 0.8.1.0, < 0.8.2a',
-    ],
-
     author = 'Leif Strand',
     author_email = 'leif@geodynamics.org',
     description = """A Python interpreter which embeds MPI.""",
     license = 'BSD',
-    url = 'http://www.geodynamics.org/cig/software/packages/cs/pythia/',
+    url = 'http://www.geodynamics.org/cig/software/pythia/',
 
 )
